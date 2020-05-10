354e1037a09817e8805e795f37e08860cc0a7118
naelamadou <naelamadou@gmail.com>
2020-05-10 18:18:59 +0000

    premiere modification

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -47,7 +47,7 @@
         <div class="container">
             <div class="row section-separator">
                 <div class="section-title col-sm-12">
-                    <h3>Featured Projects</h3>
+                    <h3>Futur Projects</h3>
                 </div>
                 <div class="col-sm-12">
                     <div class="mh-single-project-slide-by-side row">
